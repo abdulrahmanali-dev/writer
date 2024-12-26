fa2dd1d522e4c956ec5f05e301f182835132eaf7
Abdo <abdulrahman.ali6666@gmail.com>
2024-12-26 22:21:08 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,225 @@
+function _0x3f83(_0x300ec3, _0x36d07b) {
+ var _0x4f486d = _0x4f48();
+ return (
+  (_0x3f83 = function (_0x3f8387, _0x1eb15a) {
+   _0x3f8387 = _0x3f8387 - 0x135;
+   var _0xfdf80a = _0x4f486d[_0x3f8387];
+   return _0xfdf80a;
+  }),
+  _0x3f83(_0x300ec3, _0x36d07b)
+ );
+}
+function _0x4f48() {
+ var _0x22b313 = [
+  "userAgent",
+  "change",
+  "[data-fancybox]",
+  "menu_fixed",
+  "أعمال حرة",
+  "light",
+  "remove",
+  "offcanvas",
+  "each",
+  "كاتبة",
+  "current",
+  "2XGIIcR",
+  ".gallery\x20a",
+  "663747rNMLsw",
+  "addEventListener",
+  "470328nsSTXT",
+  "1309748bqXWHf",
+  "أعمال حرة",
+  "type",
+  "querySelectorAll",
+  "136PlvLZJ",
+  "data-fancybox",
+  "getElementById",
+  "16081540gonXWZ",
+  "#preloader",
+  "classList",
+  "removeClass",
+  "setAttribute",
+  "#nav",
+  "scroll",
+  "slick",
+  "init",
+  "10QeNVVK",
+  "body",
+  "16ThDkIy",
+  "title",
+  "materialScrollTop",
+  "968dVjKUq",
+  "attr",
+  ".mix:visible\x20a",
+  "click",
+  "counterUp",
+  "querySelector",
+  "mygallery",
+  "find",
+  "fasfa-moon",
+  "img",
+  "13965993aAFACP",
+  "preloaded",
+  "fancybox",
+  ".header-area",
+  "checked",
+  "980853TLPGQj",
+  "dark",
+  "4674672uWXcjT",
+  ".offcanvas",
+  "toggle",
+  "data-theme",
+  "scrollTop",
+  "image",
+  "data-caption",
+  "onePageNav",
+  "alt",
+  "addClass",
+  "next",
+ ];
+ _0x4f48 = function () {
+  return _0x22b313;
+ };
+ return _0x4f48();
+}
+(function (_0x50f2f4, _0x52e5df) {
+ var _0x42b4c3 = _0x3f83,
+  _0x189946 = _0x50f2f4();
+ while (!![]) {
+  try {
+   var _0x5eb197 =
+    (parseInt(_0x42b4c3(0x16d)) / 0x1) * (-parseInt(_0x42b4c3(0x172)) / 0x2) +
+    (parseInt(_0x42b4c3(0x155)) / 0x3) * (-parseInt(_0x42b4c3(0x143)) / 0x4) +
+    (-parseInt(_0x42b4c3(0x141)) / 0x5) * (-parseInt(_0x42b4c3(0x157)) / 0x6) +
+    (-parseInt(_0x42b4c3(0x16f)) / 0x7) * (parseInt(_0x42b4c3(0x135)) / 0x8) +
+    -parseInt(_0x42b4c3(0x150)) / 0x9 +
+    parseInt(_0x42b4c3(0x138)) / 0xa +
+    (-parseInt(_0x42b4c3(0x146)) / 0xb) * (-parseInt(_0x42b4c3(0x171)) / 0xc);
+   if (_0x5eb197 === _0x52e5df) break;
+   else _0x189946["push"](_0x189946["shift"]());
+  } catch (_0x2c51f8) {
+   _0x189946["push"](_0x189946["shift"]());
+  }
+ }
+})(_0x4f48, 0xcba74),
+ (function (_0x40f89e) {
+  "use strict";
+  var _0x33036a = _0x3f83;
+  _0x40f89e(window)["on"](_0x33036a(0x13e), function () {
+   var _0x57012e = _0x33036a;
+   _0x40f89e(this)[_0x57012e(0x15b)]() > 0x14
+    ? _0x40f89e(_0x57012e(0x153))[_0x57012e(0x160)]("menu_fixed")
+    : _0x40f89e(_0x57012e(0x153))[_0x57012e(0x13b)](_0x57012e(0x165));
+  }),
+   _0x40f89e("#offcanvasExample\x20a")[_0x33036a(0x149)](function () {
+    var _0x3df6ef = _0x33036a;
+    _0x40f89e(_0x3df6ef(0x158))[_0x3df6ef(0x169)]("hide");
+   }),
+   _0x40f89e("body")[_0x33036a(0x145)](),
+   _0x40f89e(_0x33036a(0x13d))[_0x33036a(0x15e)]({
+    currentClass: _0x33036a(0x16c),
+   }),
+   _0x40f89e(".slick-slider")[_0x33036a(0x13f)]({
+    arrows: ![],
+    dots: !![],
+    infinite: !![],
+    loop: !![],
+    autoplay: !![],
+    speed: 0x12c,
+    autoplaySpeed: 0x7d0,
+    slidesToShow: 0x3,
+    slidesToScroll: 0x3,
+    responsive: [
+     {
+      breakpoint: 0x4b0,
+      settings: {
+       slidesToShow: 0x2,
+       slidesToScroll: 0x2,
+       infinite: !![],
+       dots: !![],
+      },
+     },
+     {
+      breakpoint: 0x300,
+      settings: { slidesToShow: 0x1, slidesToScroll: 0x1 },
+     },
+     {
+      breakpoint: 0x1e0,
+      settings: { slidesToShow: 0x1, slidesToScroll: 0x1 },
+     },
+    ],
+   }),
+   _0x40f89e(".counterup")[_0x33036a(0x14a)]({ delay: 0x64, time: 0xbb8 });
+  var _0x16504f = new Typed(".element", {
+   strings: [
+    _0x33036a(0x166),
+    _0x33036a(0x173),
+    _0x33036a(0x16b)
+   ],
+   typeSpeed: 0x64,
+   backSpeed: 0x64,
+   backDelay: 0x3e8,
+   startDelay: 0x64,
+   loop: !![],
+  });
+  AOS[_0x33036a(0x140)]();
+  const _0x3eb91d = document[_0x33036a(0x14b)](
+   ".theme-switcher-label\x20input"
+  );
+  _0x3eb91d[_0x33036a(0x170)](_0x33036a(0x163), (_0xd16a4) => {
+   var _0x594119 = _0x33036a;
+   _0xd16a4["target"][_0x594119(0x154)]
+    ? document[_0x594119(0x142)][_0x594119(0x13c)](
+       _0x594119(0x15a),
+       _0x594119(0x167)
+      )
+    : document["body"]["setAttribute"]("data-theme", _0x594119(0x156));
+   let _0x86c8c5 = document[_0x594119(0x175)](".theme-buttons");
+  });
+  const _0x35a9fc = document[_0x33036a(0x137)]("toggleDark");
+  _0x35a9fc[_0x33036a(0x170)](_0x33036a(0x149), function () {
+   var _0x3dde1a = _0x33036a;
+   this[_0x3dde1a(0x13a)][_0x3dde1a(0x159)](_0x3dde1a(0x14e));
+  });
+  var _0x28429a = mixitup(".portfolio-list");
+  _0x40f89e(_0x33036a(0x16e))["attr"](_0x33036a(0x136), _0x33036a(0x14c)),
+   _0x40f89e(_0x33036a(0x16e))[_0x33036a(0x16a)](function () {
+    var _0x498463 = _0x33036a;
+    _0x40f89e(this)[_0x498463(0x147)](
+     _0x498463(0x15d),
+     _0x40f89e(this)[_0x498463(0x14d)]("img")["attr"](_0x498463(0x15f))
+    ),
+     _0x40f89e(this)["attr"](
+      _0x498463(0x144),
+      _0x40f89e(this)
+       [_0x498463(0x14d)](_0x498463(0x14f))
+       [_0x498463(0x147)](_0x498463(0x15f))
+     );
+   }),
+   _0x40f89e(_0x33036a(0x16e))[_0x33036a(0x152)](),
+   _0x40f89e(_0x33036a(0x164))[_0x33036a(0x152)]({
+    selector: _0x33036a(0x148),
+    loop: !0x0,
+    hash: !0x0,
+    transitionEffect: "slide",
+    clickContent: function (_0x3b1c38, _0x33d1c9) {
+     var _0x4291c5 = _0x33036a;
+     return (
+      _0x4291c5(0x15c) === _0x3b1c38[_0x4291c5(0x174)] && _0x4291c5(0x161)
+     );
+    },
+   });
+  var _0x41c380 = !!/Android|webOS|iPhone|iPad|iPod|BlackBerry/i["test"](
+    navigator[_0x33036a(0x162)]
+   ),
+   _0x25f2e5 = _0x40f89e(_0x33036a(0x139));
+  _0x41c380
+   ? _0x25f2e5[_0x33036a(0x168)]()
+   : (setTimeout(function () {
+      var _0x2e6d85 = _0x33036a;
+      _0x25f2e5["addClass"](_0x2e6d85(0x151));
+     }, 0x3e8),
+     setTimeout(function () {
+      _0x25f2e5["remove"]();
+     }, 0x7d0));
+ })(jQuery);
